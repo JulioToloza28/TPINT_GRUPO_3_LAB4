ef36900ac70c19a0a8f480b532cec5d39d848f32
Daniela Rubinstein <nanurubinstein@gmail.com>
2020-06-20 20:56:35 -0300

    Daniela Rubinstein: Conexion

--- TPINT_GRUPO_3_LAB4/src/daoImpl/Conexion.java
+++ TPINT_GRUPO_3_LAB4/src/daoImpl/Conexion.java
@@ -1,9 +1,44 @@
 package daoImpl;
+import java.sql.Connection;
+import java.sql.DriverManager;
 
 public class Conexion {
+	
+	public static Conexion instancia;
+	private Connection connection;
+	
+	private Conexion()
+	{
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			this.connection=DriverManager.getConnection("jdbc:mysql://localhost:3306/Sql_tpint_grupo_3","root","root");
+			this.connection.setAutoCommit(false);
+		}
+		catch(Exception ex){
+			ex.printStackTrace();
+		}
+	}
 
-	public Conexion() {
-		// TODO Auto-generated constructor stub
+	public static Conexion getConexion() {
+		
+		if(instancia==null) {
+			instancia=new Conexion();
+		}
+		return instancia;
+	}
+	
+	public Connection getSQLConexion() {
+		return this.connection;
+	}
+	
+	public void cerrarConexion() {
+		try {
+			this.connection.close();
+		}
+		catch(Exception ex) {
+			ex.printStackTrace();
+		}
+		instancia=null;
 	}
 
 }
