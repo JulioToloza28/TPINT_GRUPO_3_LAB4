5f4ca13574be162bf18f68f58be6bb98d3dc8300
rodrigo <rodrigouisguzman@gmail.com>
2020-07-22 00:03:07 -0300

    commit-rguzman-reporte

--- TPINT_GRUPO_3_LAB4/src/daoImpl/ReporteDaoImpl.java
+++ TPINT_GRUPO_3_LAB4/src/daoImpl/ReporteDaoImpl.java
@@ -10,77 +10,101 @@ import entidades.Curso;
 import entidades.Materia;
 import entidades.Reporte;
 
-public class ReporteDaoImpl implements ReporteDao{
-	private static final String obtenerDatos ="SELECT * FROM tpint_grupo_3_lab4.datos_reporte ";
-	
-	public ArrayList<Reporte> obtenerDatos(int idcurso,int idmateria, int cuatrimestre, int anio) {
+public class ReporteDaoImpl implements ReporteDao {
+	private static final String obtenerDatosNotas = "SELECT * FROM tpint_grupo_3_lab4.datos_reporte where idcurso >= 0 ";
+
+	public ArrayList<Reporte> obtenerDatosNotas(int idmateria, int cuatrimestre, int anio, int tipo) {
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
 		} catch (ClassNotFoundException e) {
 			e.printStackTrace();
 		}
-		
-		String consulta = obtenerDatos;
-		int cont=0;
-		
-		if (idcurso != 0) 
-		{
-			consulta = consulta + " adn idcurso= " + idcurso;
-			cont++;
-		}
 
-		if (idmateria != 0) 
-		{
+		String consulta = obtenerDatosNotas;
+		int cont = 0;
+
+		if (idmateria != 0) {
 			consulta = consulta + " and idMateria= " + idmateria;
 			cont++;
 		}
-		if (cuatrimestre != 0) 
-		{
+		if (cuatrimestre != 0) {
 			consulta = consulta + " and cuatrimestre= " + cuatrimestre;
 			cont++;
 		}
-		if (anio != 0) 
-		{
+		if (anio != 0) {
 			consulta = consulta + " and anio= " + anio;
 			cont++;
 		}
-		if(cont==0) 
-		{
-			consulta=obtenerDatos;
+		if (cont == 0) {
+			consulta = obtenerDatosNotas;
 		}
-		
 
 		ArrayList<Reporte> listReporte = new ArrayList<Reporte>();
 		PreparedStatement statement;
 		ResultSet resultSet;
 
-		Conexion conexion = Conexion.getConexion();
-		try {
-			statement = conexion.getSQLConexion().prepareStatement(consulta);
-			resultSet = statement.executeQuery();
-			while (resultSet.next()) {
-				Reporte reporte = new Reporte();
-				Curso curso = new Curso();
-				Materia materia = new Materia();
-				
-				reporte.setTotal_alumnos(resultSet.getInt("total_alumnos"));
-				reporte.setTotal_alumnos(resultSet.getInt("total_noAprobados"));
-				reporte.setTotal_alumnos(resultSet.getInt("total_Aprobados"));
-				
-				curso.setId(resultSet.getInt("idcurso"));
-				curso.setCuatrimestre(resultSet.getInt("cuatrimestre"));
-				curso.setAnio(resultSet.getInt("anio"));
-				reporte.setCurso(curso);
-				
-				materia.setId(resultSet.getInt("idMateria"));
-				materia.setNombre(resultSet.getString("MAteria"));
-				reporte.setMateria(materia);
-				
-				listReporte.add(reporte);
+		if (tipo == 1) {
+			Conexion conexion = Conexion.getConexion();
+			try {
+				statement = conexion.getSQLConexion().prepareStatement(consulta);
+				resultSet = statement.executeQuery();
+				while (resultSet.next()) {
+					Reporte reporte = new Reporte();
+					Curso curso = new Curso();
+					Materia materia = new Materia();
+
+					reporte.setTotal_no_aprobados(resultSet.getInt("total_noAprobados"));
+					reporte.setTotal_aprobados(resultSet.getInt("total_Aprobados"));
+
+					reporte.setTotal_alumnos(resultSet.getInt("total_alumnos"));
+
+					curso.setCuatrimestre(resultSet.getInt("cuatrimestre"));
+					curso.setAnio(resultSet.getInt("anio"));
+					reporte.setCurso(curso);
+
+					materia.setId(resultSet.getInt("idMateria"));
+					materia.setNombre(resultSet.getString("MAteria"));
+					reporte.setMateria(materia);
+
+					listReporte.add(reporte);
+				}
+			} catch (SQLException ex) {
+				ex.printStackTrace();
 			}
-		} catch (SQLException ex) {
-			ex.printStackTrace();
+			return listReporte;
+
+		} else {
+			Conexion conexion = Conexion.getConexion();
+			try {
+				statement = conexion.getSQLConexion().prepareStatement(consulta);
+				resultSet = statement.executeQuery();
+				while (resultSet.next()) {
+					Reporte reporte = new Reporte();
+					Curso curso = new Curso();
+					Materia materia = new Materia();
+
+					reporte.setTotal_alumnos_libres(resultSet.getInt("total_alumnos_libres"));
+					reporte.setTotal_alumnos_en_curso(resultSet.getInt("total_alumnos_en_curso"));
+					reporte.setTotal_alumnos_regularizados(resultSet.getInt("total_alumnos_regularizados"));
+
+					reporte.setTotal_alumnos(resultSet.getInt("total_alumnos"));
+
+					curso.setCuatrimestre(resultSet.getInt("cuatrimestre"));
+					curso.setAnio(resultSet.getInt("anio"));
+					reporte.setCurso(curso);
+
+					materia.setId(resultSet.getInt("idMateria"));
+					materia.setNombre(resultSet.getString("MAteria"));
+					reporte.setMateria(materia);
+
+					listReporte.add(reporte);
+				}
+			} catch (SQLException ex) {
+				ex.printStackTrace();
+			}
+			return listReporte;
+
 		}
-		return listReporte;
+
 	}
 }
--- TPINT_GRUPO_3_LAB4/src/entidades/Reporte.java
+++ TPINT_GRUPO_3_LAB4/src/entidades/Reporte.java
@@ -4,6 +4,9 @@ public class Reporte {
 	private int Id;
 	private Curso curso;
 	private Materia materia;
+	private int total_alumnos_libres;
+	private int total_alumnos_en_curso;
+	private int total_alumnos_regularizados;
 	private int total_no_aprobados;
 	private int total_aprobados;
 	private int total_alumnos;
@@ -27,6 +30,21 @@ public class Reporte {
 	public void setMateria(Materia materia) {
 		this.materia = materia;
 	}
+	public int getTotal_alumnos_en_curso() {
+		return total_alumnos_en_curso;
+	}
+
+	public void setTotal_alumnos_en_curso(int total_alumnos_en_curso) {
+		this.total_alumnos_en_curso = total_alumnos_en_curso;
+	}
+
+	public int getTotal_alumnos_regularizados() {
+		return total_alumnos_regularizados;
+	}
+
+	public void setTotal_alumnos_regularizados(int total_alumnos_regularizados) {
+		this.total_alumnos_regularizados = total_alumnos_regularizados;
+	}
 	public int getTotal_no_aprobados() {
 		return total_no_aprobados;
 	}
@@ -45,14 +63,21 @@ public class Reporte {
 	public void setTotal_alumnos(int total_alumnos) {
 		this.total_alumnos = total_alumnos;
 	}
+
+	public int getTotal_alumnos_libres() {
+		return total_alumnos_libres;
+	}
+
+	public void setTotal_alumnos_libres(int total_alumnos_libres) {
+		this.total_alumnos_libres = total_alumnos_libres;
+	}
+
 	@Override
-	
 	public String toString() {
-		return "Reporte [Id=" + Id + ", curso=" + curso + ", materia=" + materia + ", total_no_aprobados="
+		return "Reporte [Id=" + Id + ", curso=" + curso + ", materia=" + materia + ", total_alumnos_libres="
+				+ total_alumnos_libres + ", total_alumnos_en_curso=" + total_alumnos_en_curso
+				+ ", total_alumnos_regularizados=" + total_alumnos_regularizados + ", total_no_aprobados="
 				+ total_no_aprobados + ", total_aprobados=" + total_aprobados + ", total_alumnos=" + total_alumnos
 				+ "]";
 	}
-	
-	
-
 }
